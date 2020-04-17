0b691ed427d511d1c81f6ecf881f6eb5aaae04b4
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-04-17 19:45:31 -0300

    chore(eslint): remove function return type rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,10 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: "module" // Allows for the use of imports
-  }
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-function-return-type": "off"
+  },
 };
